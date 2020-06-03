b25454702f4aad377c530e0df0838855c02ba512
Reüel van der Steege <reuel@pronamic.nl>
2020-06-03 11:08:25 +0200

    Cleanup block save functions.

--- js/src/progress-text/save.js
+++ js/src/progress-text/save.js
@@ -1,28 +1,4 @@
-/**
- * WordPress dependencies
- */
-import { formatMoney } from '../utils';
-
 export default function save ( { attributes } ) {
-	let {
-		color,
-		raisedLabel,
-		raisedAmount,
-		contributionsLabel,
-		contributionsValue
-	} = attributes;
-
-	// Return early to use server render callback.
+	// Return `null` to use server render callback.
 	return null;
-
-	return (
-		<div className="ppfr-block ppfr-block-compact">
-			<dl className="ppfr-dl-list">
-				<dt className="ppfr-dl-list__label">{ raisedLabel }</dt>
-				<dd className="ppfr-dl-list__value" style={ { color: color } }>{ formatMoney( raisedAmount ) }</dd>
-				<dt className="ppfr-dl-list__label">{ contributionsLabel }</dt>
-				<dd className="ppfr-dl-list__value">{ parseInt( contributionsValue ).toString() }</dd>
-			</dl>
-		</div>
-	);
 }
